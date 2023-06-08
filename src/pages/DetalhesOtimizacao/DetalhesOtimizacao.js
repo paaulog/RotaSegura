a23e50644d54479a05f5a28a37b6d7f12804119d
paulo <rm93873@fiap.com.br>
2023-06-07 22:45:09 -0300

    Finalização do CSS e Integração da API do Correios

--- src/pages/DetalhesOtimizacao/DetalhesOtimizacao.js
+++ src/pages/DetalhesOtimizacao/DetalhesOtimizacao.js
@@ -1,18 +1,108 @@
 import "./DetalhesOtimizacao.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "../../components/Navigation/Navigation";
-import Footer from "../../components/Navigation/Navigation";
+import Footer from "../../components/Footer/Footer";
+import map from "../../images/map.png";
+import homeicon from "../../images/homeicon.png";
 
 const DetalhesOtimizacao = () => {
+  const [time, setTime] = useState("");
+  const [codigo, setCodigo] = useState("");
+  const [servico, setServico] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  let token =
+    "1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=LX002249507BR";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.linketrack.com/track/json?user=teste&token=${token}`
+        );
+        const jsonData = await response.json();
+        setErrorMessage(null);
+        setTime(jsonData.time);
+        setServico(jsonData.servico);
+        setCodigo(jsonData.codigo);
+      } catch (error) {
+        setErrorMessage("ATENÇÃO: Muitos pedidos (API). Tente Novamente!");
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <main>
       <Navigation />
       <h1>Otimização de Rotas</h1>
-
-      <div>
-        <Footer />
+      <div className="subtitle-rotas">
+        <p className="error-message">{errorMessage}</p>
+        <p>Quarta, 7 Jun</p>
+        <p>Código: {codigo}</p>
       </div>
-    </div>
+      <div className="main">
+        <img src={map} alt="Imagem Google Maps" className="map" />
+        <div className="previsao">
+          <h2>Previsão de chegada</h2>
+
+          <ul>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+            <li>
+              <div className="info">
+                <p>{servico}</p>
+                <p className="from">xxxxxxxx de São Paulo</p>
+              </div>
+              <span>{time}</span>
+            </li>
+          </ul>
+          <div className="address">
+            <img src={homeicon} alt="Ícone de casa" />
+            <div className="address-info">
+              <p>Endereço de entrega</p>
+              <span>R. dos Gusmões, 124 -Jd. Maria dos Santos - RJ</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 };
 
--- src/pages/SimulacaoDeConsumo/SimulacaoDeConsumo.js
+++ src/pages/SimulacaoDeConsumo/SimulacaoDeConsumo.js
@@ -1,83 +1,73 @@
 import "./SimulacaoDeConsumo.css";
 import React from "react";
 import Navigation from "../../components/Navigation/Navigation";
-import Footer from "../../components/Footer/Footer";
 const SimulacaoDeConsumo = () => {
   return (
     <div>
       <Navigation />
 
-      <section>
-        <h1>Simulação de consumo</h1>
-        <h2>
-          É importante que seu cliente conheça bem seu público alvo para que
-          você possa fazer a simulação de consumo
-        </h2>
-      </section>
+      <h1>Simulação de consumo</h1>
+      <p className="subtitle-simulacao">
+        É importante que seu cliente conheça bem seu público-alvo para que você
+        possa fazer a simulação de consumo
+      </p>
 
       <section id="formulario">
         <form>
-          <label htmlFor="">
-            <div>
-              <select>
-                <option value="" disabled selected>
-                  Selecione uma opção
-                </option>
-                <option value="opcao1">Opção 1</option>
-                <option value="opcao2">Opção 2</option>
-                <option value="opcao3">Opção 3</option>
-                <option value="opcao4">Opção 4</option>
-                <option value="opcao5">Opção 5</option>
-                <option value="opcao6">Opção 6</option>
+          <div className="form-group">
+            <div className="esquerdo">
+              <label htmlFor="quantidade">Quantidade de pessoas:</label>
+              <select id="quantidade">
+                <option value="" disabled selected></option>
+                <option value="opcao1">10</option>
+                <option value="opcao2">50</option>
+                <option value="opcao3">100</option>
+                <option value="opcao4">150</option>
+                <option value="opcao5">200</option>
+                <option value="opcao6">250</option>
               </select>
 
-              <input type="text" placeholder="Média de Idade" />
+              <label htmlFor="media">Média de idade:</label>
+              <input type="text" placeholder="" id="media" />
 
-              <select>
-                <option value="" disabled selected>
-                  Cidade
-                </option>
-                <option value="opcao1">Opção 1</option>
-                <option value="opcao2">Opção 2</option>
-                <option value="opcao3">Opção 3</option>
-                <option value="opcao4">Opção 4</option>
-                <option value="opcao5">Opção 5</option>
-                <option value="opcao6">Opção 6</option>
-              </select>
-            </div>
-            <div>
-              <select>
-                <p>Principais produtos</p>
-                <option value="opcao1">Opção 1</option>
-                <option value="opcao2">Opção 2</option>
-                <option value="opcao3">Opção 3</option>
-                <option value="opcao4">Opção 4</option>
-                <option value="opcao5">Opção 5</option>
-                <option value="opcao6">Opção 6</option>
-              </select>
-              <select>
-                <option value="opcao1">Opção 1</option>
-                <option value="opcao2">Opção 2</option>
-                <option value="opcao3">Opção 3</option>
-                <option value="opcao4">Opção 4</option>
-                <option value="opcao5">Opção 5</option>
-                <option value="opcao6">Opção 6</option>
-              </select>
-              <select>
-                <option value="opcao1">Opção 1</option>
-                <option value="opcao2">Opção 2</option>
-                <option value="opcao3">Opção 3</option>
-                <option value="opcao4">Opção 4</option>
-                <option value="opcao5">Opção 5</option>
-                <option value="opcao6">Opção 6</option>
+              <label htmlFor="cidade">Cidade:</label>
+              <select id="cidade">
+                <option value="" disabled selected></option>
+                <option value="opcao1">São Paulo</option>
               </select>
             </div>
 
-            <button>Enviar</button>
-          </label>
+            <div className="direito">
+              <label htmlFor="produtos">Principais Produtos:</label>
+              <select className="produtos">
+                <option value="" disabled selected></option>
+                <option value="opcao1">Maçã</option>
+                <option value="opcao2">Abacaxi</option>
+                <option value="opcao3">Mamão</option>
+                <option value="opcao4">Kiwi</option>
+              </select>
+
+              <select className="produtos">
+                <option value="" disabled selected></option>
+                <option value="opcao1">Cenoura</option>
+                <option value="opcao2">Beterraba</option>
+                <option value="opcao3">Abóbora</option>
+                <option value="opcao4">Repolho</option>
+              </select>
+
+              <select className="produtos">
+                <option value="" disabled selected></option>
+                <option value="opcao1">Carne</option>
+                <option value="opcao2">Peixe</option>
+                <option value="opcao3">Frango</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <button className="botao">Simular</button>
+          </div>
         </form>
       </section>
-      <Footer />
     </div>
   );
 };
